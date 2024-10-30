4ff0594728346d744e3884660f8855e81844467f
aka <akadev8@outlook.com>
2024-10-31 00:23:05 +0100

    Create setup.bat

---
+++ setup.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Setup script for additional configuration
+
+echo Running setup script...
+
+REM Example: Set additional environment variable
+set ADDITIONAL_VAR=example
+
+echo Setup script complete!
